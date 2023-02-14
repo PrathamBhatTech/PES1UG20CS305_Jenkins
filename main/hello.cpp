c1ce907acbe37b93312f7f574bdc3a91a5b57efb
Pratham Bhat <69955149+PrathamBhatTech@users.noreply.github.com>
2023-02-15 00:24:30 +0530

    some changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Added web hook";
-  cout << "test9";
+  cout << "test10";
   return 0;
 }
