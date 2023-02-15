d3080414ae030e9b2d77bcc475b0a6c9e06aa8a1
Pratham Bhat <69955149+PrathamBhatTech@users.noreply.github.com>
2023-02-15 21:16:03 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "test10";
+  cot << "test10";
   return 0;
 }
